8445d94cdcecaee1f9a37174fcb7f5c3e3689bde
Sotelo <miguel@space.bar>
2024-03-22 10:50:08 -0600

    Backend 1.0 terminado

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+// console.log(process.env)
+
+// #1 Crear el servidor de express 
+const app = express();
+
+// #6 Base de datos
+dbConnection();
+
+// #7 CORS
+app.use(cors());
+
+// #4 Directorio público
+app.use( express.static('public') );
+
+// #5 Lectura y parseo del body
+app.use( express.json() );
+
+// #3 Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+// #2 Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+})
