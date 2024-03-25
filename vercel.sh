0b2d05f7cbacba2c371831a958d670e47248e0b0
Sotelo <miguel@space.bar>
2024-03-25 12:27:10 -0600

    Especificar build

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
  
 if [[ $VERCEL_ENV == "production"  ]] ; then 
-  echo "This is our main branch"
   npm run build:production
 else 
-  echo "This is not our main branch"
   npm run build:preview
 fi
